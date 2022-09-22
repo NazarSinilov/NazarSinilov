92801b94a52a7567c6e396ad585451f492915edd
Nazar Sinilov <nsinilov.itrum@gmail.com>
2022-09-22 15:30:28 +0300

    clear

--- task17.js
+++
@@ -1,7 +0,0 @@
-/*В функцию подается несколько массивов. Вернуть один массив со всеми элементами.*/
-
-function deepArray (arr) {
-  return arr.flat(Infinity)
-}
-
-console.log(deepArray([[[2,4]], [7,"fsf", 8], ['a', 1, 3, 'd']]))
