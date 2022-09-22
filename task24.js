b596399d8a71c04e618aa87346c451dd8fbc908f
Nazar Sinilov <nsinilov.itrum@gmail.com>
2022-09-22 17:24:57 +0300

    Fixed task16

--- task24.js
+++ task24.js
@@ -5,18 +5,9 @@ Input: [4, 5, 2, 4, 1, 5, 3], ‘ASC’
 Output: [1, 2, 3, 4, 4, 5, 5]*/
 
 function sortArray(arr, methodSort) {
-    switch (methodSort) {
-        case "ASC":
-            arr.sort((a, b) => a - b)
-            break
-        case "DESC":
-            arr.sort((a, b) => b - a)
-            break
-        default:
-            return arr
-    }
-
-    return arr
+    if (methodSort === "ASC") return arr.sort((a, b) => a - b)
+    else if (methodSort === "DESC") arr.sort((a, b) => b - a)
+    else return arr
 }
 
-console.log(sortArray([4, 5, 2, 4, 1, 5, 3], "NOT SORT"));
+console.log(sortArray([4, 5, 2, 4, 1, 5, 3], "ASC"));
