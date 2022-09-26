3aec4fd36b0af1f81fc682d385655b9023067c5b
Nazar Sinilov <nsinilov.itrum@gmail.com>
2022-09-26 13:45:10 +0300

    Completed task3

---
+++ Module2.1-3/task3_M2.1-3.js
@@ -0,0 +1,29 @@
+/*
+На вход подается массив со значениями. Нужно вывести массив уникальных элементов (нет повторений в поданном массиве).
+Input: [5, 7, 6, 2, 8, 3, 5, 6, 2, 98, 13]
+Output: [7, 8, 3, 98, 13]
+ */
+
+const unique = arr => {
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        let flag = true
+        for (let j = 0; j < arr.length; j++) {
+            if (i === j){
+                continue
+            }
+            if (arr[i] === arr[j]) {
+                flag = false
+                break
+            }
+        }
+        if (flag) {
+            newArr.push(arr[i])
+        }
+    }
+
+    return newArr
+}
+
+console.log(unique([5, 7, 6, 2, 8, 3, 5, 6, 2, 98, 13]))
