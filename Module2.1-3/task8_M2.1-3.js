019dd2545c1f16a16a5a01bf23b57365fe3f873e
Nazar Sinilov <nsinilov.itrum@gmail.com>
2022-09-27 11:24:32 +0300

    Completed task8

---
+++ Module2.1-3/task8_M2.1-3.js
@@ -0,0 +1,20 @@
+/*
+Реализуйте функцию, которая параметром принимает объект.
+Выведите сформированную строку для браузера ('https://underscorejs.org') с параметрами.
+Например, { a: 4, b: 8 } => "https://underscorejs.org?a=4&b=8".
+Input: ( "https://docs.google.com", { id: "terdh673bb8", limit: 5, offset: 0 } )
+Output: "https://docs.google.com?id=terdh673bb8&limit=5&offset=0"
+*/
+
+const sumNum = (str, obj) => {
+    let arrayParams = []
+    for (let key in obj) {
+        arrayParams.push([key, "=", obj[key]].join(""))
+        arrayParams.push("&")
+    }
+    arrayParams.pop()
+
+    return str + "?" + arrayParams.join("")
+}
+
+console.log(sumNum(    "https://docs.google.com", { id: "terdh673bb8", limit: 5, offset: 0 }));
