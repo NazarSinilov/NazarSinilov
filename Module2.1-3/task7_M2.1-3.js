4f4a32de2f69837a5a8dddea487facbdfdcabbf5
Nazar Sinilov <nsinilov.itrum@gmail.com>
2022-09-27 11:07:45 +0300

    Completed task7

---
+++ Module2.1-3/task7_M2.1-3.js
@@ -0,0 +1,22 @@
+/*
+Дано число. Сложите его цифры. Если сумма получилась более 9-ти, опять сложите его цифры.
+И так, пока сумма не станет однозначным числом (9 и менее).
+Input: 345
+Output: 3
+*/
+
+const sumNum = num => {
+    let resNum = 0
+    let str = "" + num
+    let arr = str.split("")
+    for (let i = 0; i < arr.length; i++) {
+        resNum += +arr[i]
+    }
+    if (resNum > 9) {
+        return sumNum(resNum)
+    }
+
+    return resNum
+}
+
+console.log(sumNum(   345));
