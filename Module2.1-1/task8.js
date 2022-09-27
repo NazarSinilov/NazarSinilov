288cb285d52dfa07ff50bd9e05e8c7284059c079
Nazar Sinilov <nsinilov.itrum@gmail.com>
2022-09-27 09:54:47 +0300

    changed function. Add newArr

--- Module2.1-1/task8.js
+++ Module2.1-1/task8.js
@@ -3,25 +3,21 @@ Input: ([5, 2, 8, 6, 1, 9, 3, 6, 3, 7, 1], [2, 5, 1])
 Output: [5, 6, 1, 3, 6, 3, 7, 1]*/
 
 function deleteIndex(arr, arrIdx) {
-    for (let j = arrIdx.length - 1; j > 0; j--) {
-        for (let i = 0; i < j; i++) {
-            if (arrIdx[i] > arrIdx[i + 1]) {
-                let temp = arrIdx[i];
-                arrIdx[i] = arrIdx[i + 1];
-                arrIdx[i + 1] = temp;
-            }
-        }
-    }
-
-    for (let i = arr.length - 1; i >= 0; i-- ) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++ ) {
+        let flag = true
         for (let j = 0; j < arrIdx.length; j++) {
             if (i === arrIdx[j]) {
-                arr.splice( i , 1 )
+                flag = false
+                break
             }
+        }
+        if (flag) {
+            newArr.push(arr[i])
         }
     }
 
-    return arr
+    return newArr
 }
 
 console.log(deleteIndex([5, 2, 8, 6, 1, 9, 3, 6, 3, 7, 1], [2, 5, 1]));
